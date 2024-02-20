e1074cc32575de416d14eefdaa1615a3277cb82b
MBELARIF <149468330+MBELARIF@users.noreply.github.com>
2024-02-20 14:22:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,30 +1,20 @@
-from flask import Flask, rendertemplatestring, rendertemplate, jsonify
-from flask import Flask, rendertemplate, request, redirect
-from flask import json
-from urllib.request import urlopen
+from flask import Flask, render_template, request, redirect, jsonify, json
 import sqlite3
 
-app = Flask(__name__) #creating flask app name
+app = Flask(__name__)  # Création de l'application Flask
 
-@app.route('/consultationn')
-def consultation():
-   return render_template("consult.html")
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-@app.route('/lecture/')
-# Création d'une nouvelle route pour la lecture de la BDD
-@app.route("/consultation/")
+@app.route('/consultation/')
 def ReadBDD():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients;')
     data = cursor.fetchall()
     conn.close()
-#Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
 
 @app.route('/resume_1')
 def resume_1():
@@ -38,5 +28,5 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
-if(__name == "__main"):
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)  # Ajout de `debug=True` pour faciliter le débogage
